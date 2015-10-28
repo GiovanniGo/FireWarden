3c3d226e814b0cf9e00a91402fda7052467f3898
ggo <go.giovanni@gmail.com>
2015-10-28 17:33:31 +1100

    First Checkin

---
+++ FireWarden/FireWarden/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FireWarden.Startup))]
+namespace FireWarden
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
