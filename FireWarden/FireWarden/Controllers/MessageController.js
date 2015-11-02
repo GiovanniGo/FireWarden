709f2025b7965013bb293535e12e128781f35889
ggo <go.giovanni@gmail.com>
2015-11-02 18:22:21 +1100

    create MessageController.js.
    The controller mimics HomeController.cs but use Angular instead. Plan is to change existing asp.net controller to angular whenever possible.

---
+++ FireWarden/FireWarden/Controllers/MessageController.js
@@ -0,0 +1,12 @@
+﻿(function () {
+    var app = angular.module('home', []);
+
+    app.controller('MessageController', function () {
+        this.message = msg;
+    });
+
+    var msg = {
+        about: "Your application description page.",
+        contact: "Your contact page.",
+    }
+})();
