e1a17cc87179b641577ec954cae11dc67fa4afe0
ggo <go.giovanni@gmail.com>
2016-01-02 15:48:26 +1100

    Final final product

--- NABDemo/Rules/PricingRules.cs
+++ NABDemo/Rules/PricingRules.cs
@@ -22,15 +22,17 @@ namespace Rules
         private const int NEXUS9_MIN_DISCOUNT_ITEM = 4;
         private const string PRICE_KEY = "Price";
         private const string SKU_KEY = "SKU";
+
+        private Func<IProduct, string, bool> checkProduct = (IProduct x, string y) => x.Properties[SKU_KEY] == y;
+        private Func<IProduct, bool> isFree = (IProduct x) => x.Properties[PRICE_KEY] > 0;
+
         public List<IProduct> applyPricingRules(List<IProduct> productBasket, 
                                                 ICatalogService catalogService)
         {
             List<IProduct> discountedProductBasket = productBasket;
-
-
             //Buy three pay for two Apple TV Deal
             int appleTVCount = 0;
-            foreach(IProduct appleTVProduct in discountedProductBasket.Where(x => x.Properties[SKU_KEY] == "atv"))
+            foreach(IProduct appleTVProduct in discountedProductBasket.Where(x => checkProduct(x, "atv")))
             {
                 appleTVCount++;
                 if (appleTVCount % 3 == 0)
@@ -41,26 +43,26 @@ namespace Rules
 
             //Nexus9 discount
             // If nx9 is more than four pieces, set all the prices for nx9 to $499.99
-            if (discountedProductBasket.Count(x => x.Properties[SKU_KEY] == "nx9") > NEXUS9_MIN_DISCOUNT_ITEM)
+            if (discountedProductBasket.Count(x => checkProduct(x, "nx9")) > NEXUS9_MIN_DISCOUNT_ITEM)
             {
-                discountedProductBasket.Where(x => x.Properties[SKU_KEY] == "nx9").ToList().
+                discountedProductBasket.Where(x => checkProduct(x, "nx9")).ToList().
                     ForEach(y => y.Properties[PRICE_KEY] = 499.99);
             }
 
             //MacBook Pro + HDMI adapter bundle
             //Count the number of mbp and add hdm with zero cost
-            int mbpCount = discountedProductBasket.Count(x => x.Properties[SKU_KEY] == "mbp");
-            int hdmCount = discountedProductBasket.Count(x => x.Properties[SKU_KEY] == "hdm");
+            int mbpCount = discountedProductBasket.Count(x => checkProduct(x, "mbp"));
+            int hdmCount = discountedProductBasket.Count(x => checkProduct(x, "hdm"));
 
             // Apply discount to existing HDMI cables for the number of mbp purchased
             int appliedDiscount = 0;
             while (appliedDiscount < mbpCount &&
-                  discountedProductBasket.Count(x => x.Properties[SKU_KEY] == "hdm"
-                                                && x.Properties[PRICE_KEY] > 0) > 0) 
+                  discountedProductBasket.Count(x => checkProduct(x, "hdm")
+                                                && isFree(x)) > 0) 
             {
                 appliedDiscount++;
-                discountedProductBasket.First(x => x.Properties[SKU_KEY] == "hdm"
-                                                   && x.Properties[PRICE_KEY] > 0).Properties[PRICE_KEY] = 0;
+                discountedProductBasket.First(x => checkProduct(x, "hdm")
+                                                   && isFree(x)).Properties[PRICE_KEY] = 0;
             }
 
             // Bundle hdmi cables for each mbp that are standalone
